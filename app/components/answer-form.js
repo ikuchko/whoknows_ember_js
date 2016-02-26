02fc53b575723cd79405391adbf51329430330a3
Illia <ikuchko@gmail.com>
2016-02-26 11:30:34 -0800

    create form with functionality of adding answers

--- app/components/answer-form.js
+++ app/components/answer-form.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    addAnswer(question) {
+      var params = {
+        author: this.get('author'),
+        answer: this.get('answer'),
+        question: this.get('question')
+      }
+      this.set('author', '');
+      this.set('answer', '');
+      this.sendAction('addAnswer', params);
+    }
+  }
 });
